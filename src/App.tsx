0fdf53b8a645d081a73ee4a57f0301b18505ed0c
DenisReact <den.lopatinski.dev@gmail.com>
2022-11-10 00:57:44 +0200

    Add finish screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Finish from './pages/Finish';
 import Game from './pages/Game';
 import NotFound from './pages/NotFound';
 import Start from './pages/Start';
@@ -8,6 +9,7 @@ const App = () => (
     <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/finish" element={<Finish />} />
         <Route path="*" element={<NotFound />} />
     </Routes>
 );
---
+++ src/pages/Finish.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from '../components/Button/Button';
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import { setBudget, setLevelCount } from '../Redux/gameSlice';
+import styles from './Start.module.css';
+import classes from './Finish.module.css';
+import handSvg from '../imgs/Hand.svg';
+
+const Finish = () => {
+    const dispatch = useAppDispatch();
+    const currentBudget = useAppSelector(state => state.game.currentBudget);
+    // console.log()
+    // React.useEffect(() => {
+    //     console.log('hi');
+    //     return () => {
+    //         dispatch(setBudget('0'));
+    //         dispatch(setLevelCount(0));
+    //     };
+    // }, []);
+    return (
+        <main className={`${styles.startScreen} ${classes.finishScreen}`}>
+            <div className={styles.centerContent}>
+                <img src={handSvg} alt="" />
+                <div className={styles.text}>
+                    <h3>Total score:</h3>
+                    <h1>{currentBudget} earned</h1>
+                    <Button className={classes.tryAgain} href="/game">
+                        Try again
+                    </Button>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Finish;
